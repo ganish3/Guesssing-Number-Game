445bf9de02d3e5658437c51cd0bf3e81e4478540
ganish3 <ganishkar3@gmail.com>
2023-02-21 15:01:26 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,75 +1,100 @@
 let name;
 const gameStart = () => {
-  name = document.getElementById("name").value;
-  localStorage.setItem("name", name);
-  document.getElementById("welcome").innerHTML = "Welcome " + name + "!";
-  document.getElementById("gameIn").style.visibility = "visible";
-  localStorage.setItem("starting", new Date());
+    let name = document.getElementById("name").value;
+    localStorage.removeItem('name');
+    localStorage.setItem("name", name);
+    document.getElementById("welcome").innerHTML = "Welcome " + name + "!";
+    document.getElementById("gameIn").style.visibility = "visible";
+    localStorage.setItem("starting", new Date());
+    localStorage.removeItem('player');
+    const data = localStorage.data ? JSON.parse(localStorage.data) : [];
+    const check = data.filter((item) => item.name === name)
+    if (check && check.length > 0) {
+        // localStorage.setItem('data', JSON.stringify([...obj]))
+    } else {
+        const obj = {
+            name: name,
+            attempt: 0
+        }
+        localStorage.setItem('data', JSON.stringify([...data, obj]))
+    }
+
 };
 function random4Digit() {
-  return shuffle("0123456789".split("")).join("").substring(0, 4);
+    return shuffle("0123456789".split("")).join("").substring(0, 4);
 }
 
 function shuffle(o) {
-  for (
-    var j, x, i = o.length;
-    i;
-    j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
-  );
-  return o;
+    for (
+        var j, x, i = o.length;
+        i;
+        j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
+    );
+    return o;
 }
 let arr1 = random4Digit().split("");
 
 const validate = () => {
-  const numbers = document.getElementById("int").value.split("");
+    const numbers = document.getElementById("int").value.split("");
+    [6, 7, 7, 9]
+    const duplicates = [
+        ...new Set(numbers.filter((n, i) => numbers.indexOf(n) !== i)),
+    ];
 
-  const duplicates = [
-    ...new Set(numbers.filter((n, i) => numbers.indexOf(n) !== i)),
-  ];
-
-  if (duplicates.length) {
-    alert("Dont enter same number again");
-  } else {
-    let finalValue = compareArrays(arr1, numbers);
-    document.getElementById("result").innerHTML =
-      "Your Result is " + finalValue;
-
-    if (finalValue == "++++") {
-      localStorage.setItem("ending", new Date());
-      let date1 = new Date(localStorage.starting);
-      let date2 = new Date(localStorage.ending);
-      const diffInMs = date2 - date1;
-      const diffInMin = Math.floor(diffInMs / (1000 * 60));
-      document.getElementById("timing").innerHTML =
-        " Your best time is " + diffInMin + " minutes";
-    }
-
-    if (localStorage.player && localStorage.player.length > 0) {
-      let player = JSON.parse(localStorage.player);
-      let obj = { attempt: +player.attempt + 1, name: localStorage.name };
-      console.log(obj);
-      localStorage.setItem("player", JSON.stringify(obj));
+    if (duplicates.length) {
+        alert("Dont enter same number again");
     } else {
-      let obj = {
-        name: localStorage.name,
-        attempt: 1,
-      };
-      localStorage.setItem("player", JSON.stringify(obj));
+        let finalValue = compareArrays(arr1, numbers);
+        document.getElementById("result").innerHTML =
+            "Your Result is " + finalValue;
+        // let jsonObj = JSON.parse(localStorage.player)
+        // document.getElementById("attempt").innerHTML = "Your attempt " + jsonObj.attempt;
+
+        if (finalValue == "++++") {
+            localStorage.setItem("ending", new Date());
+            let date1 = new Date(localStorage.starting);
+            let date2 = new Date(localStorage.ending);
+            const diffInMs = date2 - date1;
+            const diffInMin = Math.floor(diffInMs / (1000 * 60));
+            document.getElementById("timing").innerHTML =
+                " Your best time is " + diffInMin + " minutes";
+        }
+        const data = localStorage.data ? JSON.parse(localStorage.data) : [];
+        const temp = data.map((item) => {
+            let name = document.getElementById("name").value;
+            if (item.name === name) {
+                item.attempt = item.attempt + 1
+            }
+            return item
+        })
+        localStorage.setItem('data',JSON.stringify(temp))
+        if (localStorage.player && localStorage.player.length > 0) {
+            let player = JSON.parse(localStorage.player);
+            let obj = { attempt: +player.attempt + 1, name: localStorage.name };
+            localStorage.setItem("player", JSON.stringify(obj));
+        } else {
+            let obj = {
+                name: localStorage.name,
+                attempt: 1,
+            };
+            localStorage.setItem("player", JSON.stringify(obj));
+        }
     }
-  }
 };
 
 function compareArrays(arr1, arr2) {
-  let emptArr = [];
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i]) {
-      emptArr.push("+");
-    } else if (arr1.includes(arr2[i])) {
-      emptArr.push("-");
-    } else {
-      emptArr.push("*");
+    let emptArr = [];
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] === arr2[i]) {
+            emptArr.push("+");
+        } else if (arr1.includes(arr2[i])) {
+            emptArr.push("-");
+        } else {
+            emptArr.push("*");
+        }
     }
-  }
-  console.log(arr1, arr2);
-  return emptArr.join("");
+    console.log(arr1, arr2);
+    return emptArr.join("");
 }
+
+localStorage.removeItem("player")
